542baa4e64ee9498063abba449826e08bafcbb3d
weyanan <weyanan@weyanandeMacBook-Pro.local>
2020-07-21 14:50:00 +0800

    提交

---
+++ src/Test/Test.java
@@ -0,0 +1,11 @@
+package Test;
+
+/**
+ * @author YangQi
+ * @date 2020/7/21 2:49 下午
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
